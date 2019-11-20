ecd04194aa8c71dc6114363c53187b62f8e0de17
liuxingxing <lxxwork0827@163.com>
2019-11-20 23:11:06 +0800

    Initial Commit

---
+++ swiftUI_learn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI_learn
+//
+//  Created by 刘星星 on 2019/11/20.
+//  Copyright © 2019 刘星星. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
